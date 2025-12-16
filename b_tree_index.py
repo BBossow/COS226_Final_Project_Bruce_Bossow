60afca5f444d4a34ae6a6038229922ee40b70f81
BBossow <brucebossow06@gmail.com>
2025-12-15 22:55:51 -0500

    Current progress
    Added the index fields for btree and finished the Hash table code, have the searchable fields as well as a way to insert, search, and delete.

--- b_tree_index.py
+++ b_tree_index.py
@@ -4,3 +4,20 @@ class BTreeIndex:
     def __init__(self, field):
         self.field = field
         self.tree = BTree(maxdegree = 5)
+
+    # Index fields, any input that is an int/float
+    def get_value(self, item):
+        if self.field == "release_date":
+            return item.release_date
+        elif self.field == "revenue":
+            return item.revenue
+        elif self.field == "rating":
+            return item.rating
+        elif self.field == "min_duration":
+            return item.min_duration
+        
+    
+
+    # Uses btree remove function to remove values.
+    def delete(self, item):
+        self.tree.remove(self.get_value(item))
--- hash_table.py
+++ hash_table.py
@@ -16,3 +16,60 @@ class HashTable:
     def __init__(self, field):
         self.field = field
         self.table = [None] * TABLE_SIZE
+
+    # for all searchable fields, items that are str
+    def get_value(self, item):
+        if self.field == "movie_name":
+            return item.movie_name
+        elif self.field == "genre":
+            return item.genre
+        elif self.field == "director":
+            return item.director
+        elif self.field == "production_company":
+            return item.production_company
+        elif self.field == "quote":
+            return item.quote
+        
+
+    def insert(self, item):
+        value = self.get_value(item)
+        key = hashFunction(value)
+        index = key % TABLE_SIZE
+
+        #Simular insert logic to hash hw, but using nested lists incase of duplicates to prevent collisions.
+        while True:
+            # if the index is empty is sets the nested list equal to the index
+            if self.table[index] == None:
+                self.table[index] = [item]
+                return
+            # if the dex is occupied, but is a duplicate, it appends it to the nested list
+            if self.get_value(self.table[index][0]) == value:
+                self.table[index].append(item)
+                return
+            index = (index + 1) % TABLE_SIZE
+    # essentually the same logic as the insert, but returns the index the given value is at
+    def search(self, value):
+        key = hashFunction(value)
+        index = key % TABLE_SIZE
+
+        while self.table[index] != None:
+            if self.get_value(self.table[index][0]) == value:
+                return self.table[index]
+            index = (index + 1) % TABLE_SIZE
+        return "Value not found"
+    
+    #simular logic to the previous two functions, it looks for the object at the index, even if there is multiple items in the index (duplicates) it deletes the specific object not just the first at index
+    def delete(self, item):
+        value = self.get_value(item)
+        key = hashFunction(value)
+        index = key % TABLE_SIZE
+
+        while self.table[index] != None:
+            # checks the index for the object then removes it, if it was the only one at index, it sets the index to None
+            if item in self.table[index]:
+                self.table[index].remove(item)
+                if len(self.table[index]) == 0:
+                    self.table[index] = None
+                return
+            index = (index + 1) % TABLE_SIZE
+        return "Value not found"
